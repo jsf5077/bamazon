e62db9110fe8230f1e048060510454bd3426f0d5
jsf5077 <jsf5077@gmail.com>
2019-07-11 22:11:41 -0400

    Initial commit with most of the file structures set up

---
+++ bamazon.sql
@@ -0,0 +1,47 @@
+-- CREATE DATABASE bamazon_DB;
+
+USE bamazon_DB;
+
+CREATE TABLE products (
+	item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NULL,
+    department_name VARCHAR(60) NULL,
+    price INT NULL,
+    stock_quantity INT NULL,
+    PRIMARY KEY (item_id) 
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bamazon Parrot, With 1st Gen LIRI", "Smart Home", 49.99, 500);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bamazon Parrot Speck, With 1st Gen LIRI", "Smart Home", 24.99, 500);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bamazon Flame TV, Works with LIRI", "Television & Accesories", 149.99, 150);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bamazon Flame HD 10 Tablet, Works with LIRI", "Tablets & Accessories", 199.99, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bamazon Spindle, Works with LIRI", "Tablets & Accessories", 99.99, 300);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("BamazonRoots AA 1.5 Volt Performance Alkaline Batteries - Pack of 48, Packaging May Vary", "Electronics", 13.99, 500);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("BamazonRoots AmazonBasics 12-Piece Colored Kitchen Knife Set", "Home & Kitchen", 15.99, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bamazon Ding Wi-Fi enabled Videp Doorbell, Works with LIRI", "Home & Security", 99.99, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("All-new Spindle Oracle", "Computers & Accessories", 249.99, 350);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Parrot Feather – Bring LIRI to your own speaker- Black", "Smart Home", 34.99, 1000);
+
+SELECT * FROM products;
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Runner6998!';
+	
---
+++ bamazonCustomer.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+
+var mysql = require("mysql");
+
+var keys = require("./keys.js");
+
+function Password (password) {
+    this.password = password;
+}
+
+var pw = new Password(keys.password);
+
+// var connection = mysql.createConnection({
+//     host: "localhost",
+
+//     // Your port; if not 3306
+//     port: 3306,
+
+//     // Your username
+//     user: "root",
+
+//     // Your password
+//     password: pw.password.password,
+//     database: "bamazon_DB"
+// });
+
+// connection.connect(function(err) {
+//     if (err) throw err;
+//     console.log("connected as id " + connection.threadId);
+//     queryProducts();
+// });
+
+// function queryProducts() {
+//     connection.query("SELECT * FROM prodcuts", function(err, res) {
+//         if (err) throw err;
+//         for (var i = 0; i < res.length; i++) {
+//             console.log(res[i].item_id + " | " + res[i].product_name + " | Dept:" + res[i].department_name + " | Price: $" + res[i].price + " | QTY: " + res[i].stock_quantity);
+//         }
+//         console.log("-----------------------------------");
+//     });
+
+// }
