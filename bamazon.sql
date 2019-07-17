f83d3905c71568a5c02c3688ef5a1eefd5389dd8
jsf5077 <jsf5077@gmail.com>
2019-07-16 20:40:01 -0400

    All four prompts for manager are fully functional (View Items for Sale, View Low Inventory, Add to Inventory, Add New Product) and console.logs have been refactored to be cleaner in the script

--- bamazon.sql
+++ bamazon.sql
@@ -1,45 +1,47 @@
--- CREATE DATABASE bamazon_DB;
+DROP DATABASE IF EXISTS bamazon_DB;
+
+CREATE DATABASE bamazon_DB;
 
 USE bamazon_DB;
 
--- CREATE TABLE products (
--- 	item_id INT NOT NULL AUTO_INCREMENT,
---     product_name VARCHAR(100) NULL,
---     department_name VARCHAR(60) NULL,
---     price INT NULL,
---     stock_quantity INT NULL,
---     PRIMARY KEY (item_id) 
--- );
+CREATE TABLE products (
+	item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NULL,
+    department_name VARCHAR(60) NULL,
+    price DECIMAL(10, 2) NULL,
+    stock_quantity INT NULL,
+    PRIMARY KEY (item_id) 
+);
 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Bamazon Parrot, With 1st Gen LIRI", "Smart Home", 49.99, 500);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bamazon Parrot, With 1st Gen LIRI", "Smart Home", 49.99, 500);
 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Bamazon Parrot Speck, With 1st Gen LIRI", "Smart Home", 24.99, 500);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bamazon Parrot Speck, With 1st Gen LIRI", "Smart Home", 24.99, 500);
 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Bamazon Flame TV, Works with LIRI", "Television & Accesories", 149.99, 150);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bamazon Flame TV, Works with LIRI", "Television & Accesories", 149.99, 150);
 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Bamazon Flame HD 10 Tablet, Works with LIRI", "Tablets & Accessories", 199.99, 200);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bamazon Flame HD 10 Tablet, Works with LIRI", "Tablets & Accessories", 199.99, 200);
 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Bamazon Spindle, Works with LIRI", "Tablets & Accessories", 99.99, 300);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bamazon Spindle, Works with LIRI", "Tablets & Accessories", 99.99, 300);
 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("BamazonRoots AA 1.5 Volt Performance Alkaline Batteries - Pack of 48, Packaging May Vary", "Electronics", 13.99, 500);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("BamazonRoots AA 1.5 Volt Alkaline Batteries - Pack of 48", "Electronics", 13.99, 500);
 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("BamazonRoots 12-Piece Colored Kitchen Knife Set", "Home & Kitchen", 15.99, 200);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("BamazonRoots 12-Piece Colored Kitchen Knife Set", "Home & Kitchen", 15.99, 200);
 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Bamazon Ding Wi-Fi enabled Video Doorbell, Works with LIRI", "Home & Security", 99.99, 200);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bamazon Ding Wi-Fi enabled Video Doorbell, Works with LIRI", "Home & Security", 99.99, 200);
 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("All-new Spindle Oracle", "Computers & Accessories", 249.99, 350);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("All-new Spindle Oracle", "Computers & Accessories", 249.99, 350);
 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Bamazon Parrot Feather – Bring LIRI to your own speaker- Black", "Smart Home", 34.99, 1000);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bamazon Parrot Feather – Bring LIRI to your own speaker- Black", "Smart Home", 34.99, 1000);
 
 -- UPDATE products 
 -- SET 
--- bamazonManager.js
+++ bamazonManager.js
@@ -28,16 +28,10 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-
-    console.log("Welcome Bamazon Manager!");
-    
+    console.log("Welcome Bamazon Manager #" + connection.threadId);
 
     var compileList;
     var number = 2;
-
-    
-    
 
     function logInMessage() {
         compileList = setInterval(decrement, 1000);
@@ -60,149 +54,216 @@ connection.connect(function(err) {
 });
 
 function start() {
-        function userPrompt () {
+    function userPrompt () {
+        inquirer
+        .prompt({
+            name: "prompt",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "EXIT"]
+        }).then(function(choice) {
+            switch(choice.prompt) {
+                case "View Products for Sale":
+                    viewItems()
+                    break;
+                case "View Low Inventory":
+                    viewLowInv()
+                    break;
+                case "Add to Inventory":
+                    addInv();
+                    break;
+                case "Add New Product":
+                    addProduct();
+                    break;
+                case "EXIT":
+                    console.log("\nConnection Terminated! Enjoy the rest of your day!");
+                    connection.end();
+                    break;
+            }
+        });
+    };
+    userPrompt ();
+    function viewItems() {
+        connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+            console.log(" ");
+            for (var i = 0; i < res.length; i++) {
+                console.log(" || " + (i + 1) + " || " + res[i].product_name + " || Dept: " + res[i].department_name + " || Price: $" + res[i].price + " || QTY: " + res[i].stock_quantity);
+                console.log("------------------------------------------------------------------------------------------------------------------------");
+                console.log("------------------------------------------------------------------------------------------------------------------------");
+            
+
+            }
+            userPrompt ()
+        });
+    }
+    function viewLowInv() {
+        connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+            console.log(" ");
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].stock_quantity <= 50) {
+                console.log(" || " + (i + 1) + " || " + res[i].product_name + " || Dept: " + res[i].department_name + " || Price: $" + res[i].price + " || QTY: " + res[i].stock_quantity);
+                console.log("------------------------------------------------------------------------------------------------------------------------");
+                console.log("------------------------------------------------------------------------------------------------------------------------");
+                }
+            }
+            userPrompt ()
+        });
+    };
+
+    function addInv() {
+        connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+
             inquirer
-            .prompt({
-                name: "prompt",
-                type: "list",
-                message: "What would you like to do?",
-                choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "EXIT"]
-            }).then(function(choice) {
-                switch(choice.prompt) {
-                    case "View Products for Sale":
-                        viewItems()
-                        break;
-                    case "View Low Inventory":
-                        viewLowInv()
-                        break;
-                    case "Add to Inventory":
-                        addInv();
-                        break;
-                    case "Add New Product":
-                        console.log("add new product")
-                        break;
-                    case "EXIT":
-                        console.log(" ");
-                        console.log("Connection Terminated! Enjoy the rest of your day!");
-                        connection.end();
-                        break;
+            .prompt([{
+                name: "whichItem",
+                type: "input",
+                message: "Please enter the product item number that you'd like to add inventory to."
+            }, {
+                name:"howMany",
+                type: "input",
+                message: "Please enter the quantity that you would like to add."
                 }
-            });
-        };
-        userPrompt ();
-        function viewItems() {
-            connection.query("SELECT * FROM products", function(err, res) {
-                if (err) throw err;
-                console.log(" ");
-                console.log(" ");
-                for (var i = 0; i < res.length; i++) {
-                    console.log(" || " + (i + 1) + " || " + res[i].product_name + " || Dept: " + res[i].department_name + " || Price: $" + res[i].price + " || QTY: " + res[i].stock_quantity);
-                    console.log("------------------------------------------------------------------------------------------------------------------------");
-                    console.log("------------------------------------------------------------------------------------------------------------------------");
-                
+            ])
+            .then(function(answer) {
+                var item = answer.whichItem - 1;
+                var invItem = item + 1;
 
-                }
-                userPrompt ()
-            });
-        }
-        function viewLowInv() {
-            connection.query("SELECT * FROM products", function(err, res) {
-                if (err) throw err;
-                for (var i = 0; i < res.length; i++) {
-                    if (res[i].stock_quantity <= 50) {
-                    console.log(" || " + (i + 1) + " || " + res[i].product_name + " || Dept: " + res[i].department_name + " || Price: $" + res[i].price + " || QTY: " + res[i].stock_quantity);
-                    console.log("------------------------------------------------------------------------------------------------------------------------");
-                    console.log("------------------------------------------------------------------------------------------------------------------------");
-                    }
-                }
-                userPrompt ()
-            });
-        }
-    
-        function addInv() {
-            connection.query("SELECT * FROM products", function(err, res) {
-                if (err) throw err;
-    
-                inquirer
-                .prompt([{
-                    name: "whichItem",
-                    type: "input",
-                    message: "Please enter the product item number that you'd like to add inventory to."
-                }, {
-                    name:"howMany",
-                    type: "input",
-                    message: "Please enter the quantity that you would like to add."
-                    }
-                ])
-                .then(function(answer) {
-                    var item = answer.whichItem - 1;
-                    var invItem = item + 1;
+                if (!item || invItem > res.length || invItem <= 0) {
+                    console.log("\nINVALID INPUT\nPlease be sure that you've entered the number that matches your item selection.\n")
+                    addInv(); 
 
-                    if (!item || invItem > res.length || invItem <= 0) {
-                        console.log(" ");
-                        console.log("INVALID INPUT")
-                        console.log("Please be sure that you've entered the number that matches your item selection.")
-                        console.log(" ");
-                        addInv(); 
+                } else {
+                    var qty = parseInt(answer.howMany);
+                    var itemInvNum = parseInt(res[item].stock_quantity);
+                if (Number.isInteger(+qty)) {
+                    var newQty = itemInvNum + qty;
+                    var purchasedItemID = res[item].item_id;
+                    if (item <= res.length) {
+                        console.log ("\nYou have chosen "+res[item].product_name + " and would like to update the inventory by a quantity of " + qty + "\n");
 
-                    } else {
-                        var qty = parseInt(answer.howMany);
-                        var itemInvNum = parseInt(res[item].stock_quantity);
-                    if (Number.isInteger(+qty)) {
-                        var newQty = itemInvNum + qty;
-                        var purchasedItemID = res[item].item_id;
-                        if (item <= res.length) {
-                            console.log(" ");
-                            console.log ("You have chosen "+res[item].product_name + " and would like to update the inventory by a quantity of " + qty);
+                        inquirer
+                        .prompt({
+                            name: "confirmOrder",
+                            type: "list",
+                            message: "Please confirm transaction.",
+                            choices: ["YES", "NO"]
+                        }).then(function(choice) {
+                            switch(choice.confirmOrder) {
+                                case "YES":
+                                                            
+                                updateProduct(newQty, purchasedItemID);
+                                userPrompt();
+                                    break;
 
-                            inquirer
-                            .prompt({
-                                name: "confirmOrder",
-                                type: "list",
-                                message: "Please confirm transaction.",
-                                choices: ["YES", "NO"]
-                            }).then(function(choice) {
-                                switch(choice.confirmOrder) {
-                                    case "YES":
-                                                                
-                                    updateProduct(newQty, purchasedItemID);
+                                case "NO":
+                                    console.log("\ntransaction cancelled\n");
                                     userPrompt();
-                                        break;
-
-                                    case "NO":
-                                        console.log(" ");
-                                        console.log("transaction cancelled");
-                                        console.log("");
-                                        userPrompt();
-                                        break;
-                                }
-                                
-                            });
-                        } else {
-                            console.log(" ");
-                            console.log("INVALID INPUT")
-                            console.log("Please be sure that you've entered the number that matches your item selection.")
-                            console.log(" ");
-                            addInv();
-                        }
-                    } else {
-                        console.log(" ");
-                        console.log("INVAID INPUT.")
-                        console.log("Please be sure that you've entered a number for quantity.")
-                        console.log(" ");
-                        userPrompt(); 
-                    } 
-                    function updateProduct(newQty, invItem) {
-                        connection.query( "UPDATE products SET stock_quantity=? WHERE item_id=?",
-                            [newQty, invItem],
-                            function(err) {
-                                if (err) throw err;
+                                    break;
                             }
-                        );
+                            
+                        });
+                    } else {
+                        console.log("\nINVALID INPUT\nPlease be sure that you've entered the number that matches your item selection.\n")
+                        addInv();
                     }
-                    }
-                });
+                } else {
+                    console.log("\nINVAID INPUT.\nPlease be sure that you've entered a number for quantity.\n")
+                    userPrompt(); 
+                } 
+                function updateProduct(newQty, invItem) {
+                    connection.query( "UPDATE products SET stock_quantity=? WHERE item_id=?",
+                        [newQty, invItem],
+                        function(err) {
+                            if (err) throw err;
+                        }
+                    );
+                }
+                }
             });
-        }
- 
+        });
+    }
+
+    function addProduct() {
+        connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+            
+            var deptArr = []; 
+            for (i = 0; i < res.length; i++) {
+                deptArr.push(res[i].department_name)
+            }
+            let uDeptArr = [...new Set(deptArr)];
+
+        inquirer
+            .prompt([{
+                name: "newItem",
+                type: "input",
+                message: "Please enter the name of the new product: "
+            },{
+                name: "itemDept",
+                type: "list",
+                message: "Please choose the department of the new product",
+                choices: uDeptArr
+
+            },{
+                name: "itemPrice",
+                type: "input",
+                message: "What is the price of the new product?(ENTER NUMBERS ONLY) $"
+            },{
+                name: "itemStock",
+                type: "input",
+                message: "What is the initial stock quantity of the new product?(ENTER NUMBERS ONLY) "
+            }])
+            .then(function(answer) {  
+                var newProduct = answer.newItem;
+                var prodDept = answer.itemDept;
+                var prodPrice = parseFloat(answer.itemPrice);
+                var prodStock = parseInt(answer.itemStock);
+
+                if ( isNaN(prodPrice) || isNaN(prodStock)) {
+
+                    console.log(" \n INVALID INPUT.\n Please review your answers and try again.\n ")
+                    userPrompt();
+                    
+                } else {
+                    console.log("\nNEW ITEM:")
+                    console.log("NAME: "+newProduct);
+                    console.log("DEPT: "+prodDept);
+                    console.log("PRICE: $"+prodPrice);
+                    console.log("INITIAL STOCK QUANTITY: "+prodStock+"\n");
+                    
+                    inquirer
+                    .prompt({
+                        name: "confirmOrder",
+                        type: "list",
+                        message: "Please confirm new product.",
+                        choices: ["YES", "NO"]
+                    }).then(function(choice) {
+                        switch(choice.confirmOrder) {
+                            case "YES":
+                                connection.query(
+                                    "INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (?, ?, ?, ?);",
+                                    [newProduct, prodDept, prodPrice, prodStock],
+                                    function(err) {
+                                        if (err) throw err;
+                                    }
+                                )
+
+                                // INSERT INTO products (product_name, department_name, price, stock_quantity)
+                                // VALUES ("Bamazon Parrot Feather – Bring LIRI to your own speaker- Black", "Smart Home", 34.99, 1000);
+                                    
+                                break;
+
+                            case "NO":
+                                console.log("\ntransaction cancelled\n");
+                                userPrompt();
+                                break;
+                        }
+                    });
+                }
+            });
+        });    
+    }
 }
